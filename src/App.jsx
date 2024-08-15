3d7062a119e693988aa90287c9eb0e5a6117afce
Ramirez Diaz Carlos <cer_79@hotmail.com>
2024-08-15 18:48:40 -0300

    Practica 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,148 @@
-import Books from './components/books/Books';
+//import Books from './components/books/Books';
 
 function App() {
 
-  const books = [
+  // Dada la siguiente watchlist, crear una función que tome el array original
+  // y devuelva un arreglo de objetos que solo contenga las películas:
+  // 1. Dirigidas por Cristopher Nolan.
+  // 2. Con un imdbRating > 8.0
+  // El arreglo que se devuelve SOLO DEBE CONTENER el nombre de la película, ningún otra información
+  // Es obligatorio para este CC resolverlo con la función map()
+
+  let watchList = [
     {
-      bookTitle: "100 años de soledad",
-      bookAuthor: "Gabriel García Marquez",
-      bookRating: Array(5).fill("*"),
-      pageCount: 410,
-      imageUrl: "https://images.cdn3.buscalibre.com/fit-in/360x360/61/8d/618d227e8967274cd9589a549adff52d.jpg",
+      Title: 'Inception',
+      Year: '2010',
+      Rated: 'PG-13',
+      Released: '16 Jul 2010',
+      Runtime: '148 min',
+      Genre: 'Action, Adventure, Crime',
+      Director: 'Christopher Nolan',
+      Writer: 'Christopher Nolan',
+      Actors: 'Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy',
+      Plot:
+        'A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.',
+      Language: 'English, Japanese, French',
+      Country: 'USA, UK',
+      Awards: 'Won 4 Oscars. Another 143 wins & 198 nominations.',
+      Poster:
+        'http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
+      Metascore: '74',
+      imdbRating: '8.8',
+      imdbVotes: '1,446,708',
+      imdbID: 'tt1375666',
+      Type: 'movie',
+      Response: 'True',
     },
     {
-      bookTitle: "Asesinato en el Orient Express",
-      bookAuthor: "Agatha Christie",
-      bookRating: Array(4).fill("*"),
-      pageCount: 256,
-      imageUrl: "https://m.media-amazon.com/images/I/71RFyM95qwL._AC_UF1000,1000_QL80_.jpg",
+      Title: 'Interstellar',
+      Year: '2014',
+      Rated: 'PG-13',
+      Released: '07 Nov 2014',
+      Runtime: '169 min',
+      Genre: 'Adventure, Drama, Sci-Fi',
+      Director: 'Christopher Nolan',
+      Writer: 'Jonathan Nolan, Christopher Nolan',
+      Actors: 'Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow',
+      Plot:
+        "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+      Language: 'English',
+      Country: 'USA, UK',
+      Awards: 'Won 1 Oscar. Another 39 wins & 132 nominations.',
+      Poster:
+        'http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg',
+      Metascore: '74',
+      imdbRating: '8.6',
+      imdbVotes: '910,366',
+      imdbID: 'tt0816692',
+      Type: 'movie',
+      Response: 'True',
     },
     {
-      bookTitle: "Las dos torres",
-      bookAuthor: "J.R.R Tolkien",
-      bookRating: Array(5).fill("*"),
-      pageCount: 352,
-      imageUrl: "https://m.media-amazon.com/images/I/A1y0jd28riL._AC_UF1000,1000_QL80_.jpg",
+      Title: 'The Dark Knight',
+      Year: '2008',
+      Rated: 'PG-13',
+      Released: '18 Jul 2008',
+      Runtime: '152 min',
+      Genre: 'Action, Adventure, Crime',
+      Director: 'Christopher Nolan',
+      Writer:
+        'Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)',
+      Actors: 'Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine',
+      Plot:
+        'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.',
+      Language: 'English, Mandarin',
+      Country: 'USA, UK',
+      Awards: 'Won 2 Oscars. Another 146 wins & 142 nominations.',
+      Poster:
+        'http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg',
+      Metascore: '82',
+      imdbRating: '9.0',
+      imdbVotes: '1,652,832',
+      imdbID: 'tt0468569',
+      Type: 'movie',
+      Response: 'True',
     },
     {
-      bookTitle: "50 sombras de Grey",
-      bookAuthor: "E.L James",
-      bookRating: Array(1).fill("*"),
-      pageCount: 514,
-      imageUrl: "https://prodimage.images-bn.com/pimages/9781728260839_p0_v2_s1200x630.jpg",
+      Title: 'Batman Begins',
+      Year: '2005',
+      Rated: 'PG-13',
+      Released: '15 Jun 2005',
+      Runtime: '140 min',
+      Genre: 'Action, Adventure',
+      Director: 'Christopher Nolan',
+      Writer:
+        'Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)',
+      Actors: 'Christian Bale, Michael Caine, Liam Neeson, Katie Holmes',
+      Plot:
+        'After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.',
+      Language: 'English, Urdu, Mandarin',
+      Country: 'USA, UK',
+      Awards: 'Nominated for 1 Oscar. Another 15 wins & 66 nominations.',
+      Poster:
+        'http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg',
+      Metascore: '70',
+      imdbRating: '8.3',
+      imdbVotes: '972,584',
+      imdbID: 'tt0372784',
+      Type: 'movie',
+      Response: 'True',
+    },
+    {
+      Title: 'Avatar',
+      Year: '2009',
+      Rated: 'PG-13',
+      Released: '18 Dec 2009',
+      Runtime: '162 min',
+      Genre: 'Action, Adventure, Fantasy',
+      Director: 'James Cameron',
+      Writer: 'James Cameron',
+      Actors: 'Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang',
+      Plot:
+        'A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.',
+      Language: 'English, Spanish',
+      Country: 'USA, UK',
+      Awards: 'Won 3 Oscars. Another 80 wins & 121 nominations.',
+      Poster:
+        'http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg',
+      Metascore: '83',
+      imdbRating: '7.9',
+      imdbVotes: '876,575',
+      imdbID: 'tt0499549',
+      Type: 'movie',
+      Response: 'True',
     },
   ];
 
-  return (
-    <div>
-      <h2>Books Champion App</h2>
-      <p>¡Quiero leer libros!</p>
-      <Books books={books} />
-    </div>
-  );
+  const returnMovieTitle = (watchlist) => {
+    return watchlist
+      .filter(movie => movie.Director === "Christopher Nolan" && movie.imdbRating > 8.0)
+      .map(movie => movie.Title);
+  }
+
+  const titlesArray = returnMovieTitle(watchList);
+  console.log(titlesArray);
+
 }
 
 
