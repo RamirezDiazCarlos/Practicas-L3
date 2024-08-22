2a438f9a23931890d73f3a8e90d685818ca5104a
Carlos Ramirez Diaz <cerd469@gmail.com>
2024-08-21 21:37:29 -0300

    Ejer 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 
-import Table from './components/table/Table'
+import Beers from './components/beers/Beers'
 
 function App() {
 
-  const netIncomes = [
-    { brand: 'McDonalds', income: 1291283 },
-    { brand: 'Burger King', income: 1927361 },
-    { brand: 'KFC', income: 1098463 },
-  ];
+  
 
-  const totalIncome = netIncomes.reduce((total, company) => total + company.income, 0);
-  const averageIncome = totalIncome / netIncomes.length;
-
+  
   return (
-    <div>
-      <h1>Net Incomes</h1>
-      <Table netIncomes={netIncomes} />
-      <p>Average Net Income: {averageIncome}</p>
-    </div>
+
+    <Beers/>
+    
   );
 }
 
